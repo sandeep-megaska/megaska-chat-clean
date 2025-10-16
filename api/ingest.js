d971143770cd506ecf689cf18f4a66dc35f3c707
sandeep-megaska <sandeep.lalu@gmail.com>
2025-10-16 23:44:49 +0300

    Create ingest.js

---
+++ api/ingest.js
@@ -0,0 +1,8 @@
+const SITEMAPS = [
+  'https://megaska.com/sitemap.xml',
+  'https://megaska.com/sitemap_pages_1.xml',
+  'https://megaska.com/sitemap_products_1.xml',
+  // add _2, _3 if they exist:
+  // 'https://megaska.com/sitemap_products_2.xml',
+];
+const MAX_PAGES = 500; // was 120
